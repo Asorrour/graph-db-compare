c5e5dbdf6accbfd59fb813cb38323d49c457b81d
asorrour <asorrour@intersog.com>
2025-01-09 17:05:18 -0800

    Remove Main.java, not needed

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.Random;
-
-public class Main {
-    public static void main(String[] args) {
-        new Random().nextInt(2);
-        System.out.println("Hello, World!");
-    }
-}
